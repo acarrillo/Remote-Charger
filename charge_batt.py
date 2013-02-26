cb97e5367dc322a27e37869507608a4a073905a6
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-02-26 16:38:41 -0500

    Add error handling to keep stream uninterrupted

--- charge_batt.py
+++ charge_batt.py
@@ -2,25 +2,43 @@
 
 from socket import socket
 import xml.etree.ElementTree as ET
+import time
 
 host = '128.36.14.59'
 port = 80
 path = "/lxi/infomation.xml"
-xmlmessage = "0"
 
-s = socket()
-s.connect((host, port))
-s.send("POST %s HTTP/1.1\r\n" % path)
-s.send("Host: %s\r\n" % host)
-s.send("Content-Type: text/xml\r\n")
-s.send("Content-Length: %d\r\n\r\n" % len(xmlmessage))
-s.send(xmlmessage)
-data=""
-for line in s.makefile():
-    #print line
-    data += line
-    s.close()
-xmldat = data.split("\n")
-print xmldat[7]
-#tree=ET.fromstring(xmldat[len(xmldat)-1])
-#print tree
+last_voltage = 0
+last_set = 0
+
+def connectServ(message):
+    s = socket()
+    s.connect((host, port))
+    s.send("POST %s HTTP/1.1\r\n" % path)
+    s.send("Host: %s\r\n" % host)
+    s.send("Content-Type: text/xml\r\n")
+    s.send("Content-Length: %d\r\n\r\n" % len(message))
+    s.send(message)
+    data=""
+    for line in s.makefile():
+        #print line
+        data += line
+        s.close()
+    result = data.split("\n")
+    xmldat = result[7]
+    return xmldat
+
+
+while 1:
+    xmldat = connectServ('0')
+    #print xmldat,"\n"
+    try:
+        root=ET.fromstring(xmldat)
+        last_voltage = float(root[11].text)
+        last_set = float(root[14].text)
+    except ExpatError:
+        pass
+    print "Read voltage\t\t",last_voltage
+    print "Setpoint voltage\t",last_set
+
+    time.sleep(1)
