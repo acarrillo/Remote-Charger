cf63a95f4a67a6309af0f81da1e01173d441d76c
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-02-26 17:59:50 -0500

    Implement progress bar

--- charge_batt.py
+++ charge_batt.py
@@ -3,6 +3,7 @@
 from socket import socket
 import xml.etree.ElementTree as ET
 import time
+import sys
 
 host = '128.36.14.59'
 port = 80
@@ -10,6 +11,9 @@ path = "/lxi/infomation.xml"
 
 last_voltage = 0
 last_set = 0
+progBarTicks = 0
+ticksToDraw = 0
+progBarLen = 50
 
 def connectServ(message):
     s = socket()
@@ -28,6 +32,22 @@ def connectServ(message):
     xmldat = result[7]
     return xmldat
 
+def updateProgressBar(lv):
+    global progBarLen
+    global progBarTicks
+    for x in range(0,progBarLen+1):
+        sys.stdout.write('\b') # Flush.
+    fracDone = lv/4.2
+    ticksToDraw = int(progBarLen*fracDone)
+    spaceToDraw = progBarLen-ticksToDraw
+    for x in range(0,ticksToDraw):
+        sys.stdout.write('#') # Draw a tick.
+        progBarTicks += 1
+    for x in range(0,spaceToDraw):
+        sys.stdout.write(' ') # Draw a space.
+    sys.stdout.write(']') # Close prog bar.
+
+
 #Initialize power supply
 connectServ('4097') # Press 'P25V' to select the P25V channel
 connectServ('16449') # Press '4'
@@ -42,17 +62,17 @@ connectServ('12321') # Press 'A'
 connectServ('8241') # Press 'ON' for P25V
 
 # Do feedback loop
+sys.stdout.write("Voltage [                                                  ]")
 while last_voltage < 4.2:
     xmldat = connectServ('0')
-    #print xmldat,"\n"
     try:
         root=ET.fromstring(xmldat)
         last_voltage = float(root[11].text)
         last_set = float(root[14].text)
     except:
         pass # TODO: Debug why it occasionally fails
-    print "Read voltage\t\t",last_voltage
-    print "Setpoint voltage\t",last_set
+    updateProgressBar(last_voltage)
+    #print "Read voltage\t\t",last_voltage
 
     time.sleep(1)
-connectServ('8241') # Press 'ON' for P25V
+connectServ('8241') # Press 'OFF' for P25V
